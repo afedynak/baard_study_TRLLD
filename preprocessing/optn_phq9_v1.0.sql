7bc413b9e086043466c90ecd926896de79f7ab24
admin <admin@admins-Laptop.local>
2025-03-19 19:20:57 -0400

    Updated scripts

--- preprocessing/optn_phq9_v1.0.sql
+++ preprocessing/optn_phq9_v1.0.sql
@@ -1,21 +1,36 @@
+-- Author: Amber Fedynak
+-- Date: March 11, 2025
 USE baard;
 
 SELECT 
     record_id,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS baseline_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS baseline_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week2_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week2_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week4_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week4_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week6_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week6_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week8_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week8_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week10_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week10_score
+    
+    -- For baseline_arm_8
+    CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_date END AS baseline_date,
+    CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_total END AS baseline_score,
+    
+    -- For week_2_arm_8
+    CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_date END AS week2_date,
+    CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_total END AS week2_score,
+
+    -- For week_4_arm_8
+    CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_date END AS week4_date,
+    CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_total END AS week4_score,
+
+    -- For week_6_arm_8
+    CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_date END AS week6_date,
+    CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_total END AS week6_score,
+
+    -- For week_8_arm_8
+    CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_date END AS week8_date,
+    CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_total END AS week8_score,
+
+    -- For week_10end_arm_8
+    CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_date END AS week10_date,
+    CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_total END AS week10_score
+
 FROM 
-    optn_phq9_camh_20250228
+    baard.optn_phq9
 WHERE 
     redcap_event_name IN ("baseline_arm_8", "week_2_arm_8", "week_4_arm_8", "week_6_arm_8", "week_8_arm_8", "week_10end_arm_8")
 GROUP BY 
@@ -29,58 +44,3 @@ HAVING
         SUM(CASE WHEN redcap_event_name = "week_8_arm_8" THEN 1 ELSE 0 END) > 0 OR
         SUM(CASE WHEN redcap_event_name = "week_10end_arm_8" THEN 1 ELSE 0 END) > 0
     );
-
-
-SELECT 
-    record_id,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS baseline_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "baseline_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS baseline_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week2_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_2_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week2_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week4_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_4_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week4_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week6_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_6_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week6_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week8_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_8_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week8_score,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_date END ORDER BY phq9_date SEPARATOR ', ') AS week10_date,
-    GROUP_CONCAT(CASE WHEN redcap_event_name = "week_10end_arm_8" THEN phq9_total END ORDER BY phq9_total SEPARATOR ', ') AS week10_score
-FROM 
-    optn_phq9_camh_20250228
-WHERE redcap_event_name IN ("baseline_arm_8", "week_2_arm_8", "week_4_arm_8", "week_6_arm_8", "week_8_arm_8", "week_10end_arm_8")
-GROUP BY 
-    record_id;
-
-SELECT redcap_event_name, COUNT(*)
-FROM optn_phq9_camh_20250228
-GROUP BY redcap_event_name;
-
-SELECT * 
-FROM optn_phq9_camh_20250228
-WHERE redcap_event_name = "time_point_1_arm_9";
--- 192
-
-SELECT * 
-FROM optn_phq9_camh_20250228
-WHERE redcap_event_name = "extra_visit_2_acut_arm_8";
--- 192
-
-SELECT DISTINCT 
-    a.record_id,
-    a.redcap_event_name AS baseline_event,
-    a.phq9_date AS baseline_date,
-    a.phq9_total AS baseline_score,
-    b.redcap_event_name AS week10_event,
-    b.phq9_date AS week10_date,
-    b.phq9_total AS week10_score
-FROM 
-    optn_phq9_camh_20250228 AS a
-JOIN 
-    optn_phq9_camh_20250228 AS b
-    ON a.record_id = b.record_id
-WHERE 
-    a.redcap_event_name = 'baseline_arm_8'
-    AND b.redcap_event_name = 'week_10end_arm_8';
--- 2 duplicates
-
-
