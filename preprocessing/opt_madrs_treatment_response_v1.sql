6a24bc958aa36b74cdb3637a4b326deb55e01062
admin <admin@admins-Laptop.local>
2025-04-17 18:45:39 -0400

    Added scripts

---
+++ preprocessing/opt_madrs_treatment_response_v1.sql
@@ -0,0 +1,58 @@
+-- Author: Amber Fedynak
+-- Date: March 11, 2025
+USE baard;  
+    
+SELECT 
+    upper(record_id),
+    
+    -- Baseline date and score with GROUP_CONCAT
+    GROUP_CONCAT(
+        CASE 
+            WHEN redcap_event_name = "baseline_arm_1" THEN madrs_date 
+            ELSE NULL 
+        END
+        ORDER BY madrs_date
+        SEPARATOR ', '
+    ) AS baseline_date,
+
+    GROUP_CONCAT(
+        CASE 
+            WHEN redcap_event_name = "baseline_arm_1" THEN madrs_tot_scr 
+            ELSE NULL 
+        END
+        ORDER BY madrs_tot_scr
+        SEPARATOR ', '
+    ) AS baseline_score,
+
+    -- Week 10 date and score with GROUP_CONCAT
+    GROUP_CONCAT(
+        CASE 
+            WHEN redcap_event_name = "step_1_week_10_end_arm_2" THEN madrs_date 
+            ELSE NULL 
+        END
+        ORDER BY madrs_date
+        SEPARATOR ', '
+    ) AS week10_date,
+
+    GROUP_CONCAT(
+        CASE 
+            WHEN redcap_event_name = "step_1_week_10_end_arm_2" THEN madrs_tot_scr 
+            ELSE NULL 
+        END
+        ORDER BY madrs_tot_scr
+        SEPARATOR ', '
+    ) AS week10_score
+    
+FROM 
+    madras.madrs_1_24_25_yi
+WHERE 
+    redcap_event_name IN ("baseline_arm_1", "step_1_week_10_end_arm_2")
+GROUP BY 
+    record_id
+HAVING 
+    SUM(CASE WHEN redcap_event_name = "baseline_arm_1" THEN 1 ELSE 0 END) > 0 
+    AND (
+        SUM(CASE WHEN redcap_event_name = "step_1_week_10_end_arm_2" THEN 1 ELSE 0 END) > 0
+    );
+
+
---
+++ preprocessing/optn_madrs_treatment_response_v1.sql
@@ -0,0 +1,19 @@
+-- Author: Amber Fedynak
+-- Date: April 17, 2025
+USE baard;
+
+SELECT DISTINCT
+    a.record_id,
+    a.madrs_date AS baseline_date,
+    a.madrs_tot_scr AS baseline_score,
+    b.madrs_date AS week10_date,
+    b.madrs_tot_scr AS week10_score
+FROM 
+    optn_madrs_20250228 AS a
+JOIN 
+    optn_madrs_20250228 AS b
+    ON a.record_id = b.record_id
+WHERE 
+    a.redcap_event_name = 'baseline_arm_8'
+    AND b.redcap_event_name = 'week_10end_arm_8';
+
